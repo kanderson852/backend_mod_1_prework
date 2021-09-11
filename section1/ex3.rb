a4d9b7a6161c055ecbe7cd053e6011fcd672cfbb
Kelly Anderson <kean9528@colorado.edu>
2021-09-11 13:46:17 -0600

    Complete practice 3

---
+++ section1/ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens"
+
+puts "Hens #{Float(25 +30 / 6)}" #clalculates Hens
+puts "roosters #{Float(100 - 25 * 3 % 4)}" #Calculates roosters
+
+puts "Now I will count the eggs:"
+
+puts Float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #Counts eggs
+
+puts "Is it true that 3 + 2 < 5 - 7?" #Asks a math question
+
+puts 3 + 2 < 5 - 7 #Checks if it is true
+
+puts "What is 3 + 2? #{Float(3 + 2)}" #Calculations
+puts "What is 5 - 7? #{Float(5 - 7)}" #Calculations
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{(5 > -2)}" #Calculations
+puts "Is it greater or equal? #{(5 >= -2)}" #Calculations
+puts "Is it less or equal? #{(5 <= -2)}" #Calculations
