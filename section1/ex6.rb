54c5c007bfe7f603e8ca37fa0e129b798741f452
Kelly Anderson <kean9528@colorado.edu>
2021-09-11 14:31:06 -0600

    Complete exercise 6

---
+++ section1/ex6.rb
@@ -0,0 +1,24 @@
+#defining variables
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+#prints the variables as they are
+puts x
+puts y
+#prints the variable wrapped in text
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+#sets up the joke
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#prints the joke as false
+puts joke_evaluation
+#sets up a combination of strings
+w = "This is the left side of..."
+e = "a string with a right side."
+#combines them
+puts w + e
+#makes a longer string because it adds the contents of both strings together on one Line
+# '' and "" both work because they do the same thing
